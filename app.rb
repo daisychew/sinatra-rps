2a982c0f4b9fd6f81d1a2cab3d4ffcf0f4dbf4a2
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-02-06 22:14:41 +0000

    finished everything; before rake grade

--- app.rb
+++ app.rb
@@ -27,12 +27,28 @@ get("/paper") do
   @comp_move = moves.sample
 
   if @comp_move == "rock"
-    @outcome = "tied"
-  elsif @comp_move == "paper"
-    @outcome = "lost"
-  else
     @outcome = "won"
+  elsif @comp_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
   end
 
-  erb(:zebra)
+  erb(:giraffe)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+  erb(:cow)
 end
